b756c1625276f1faf5415f41b74e0b010df10c65
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-23 18:38:04 -0300

    chore: config and install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "no-shadow": "off",
+    quotes: "off",
+  },
+};
