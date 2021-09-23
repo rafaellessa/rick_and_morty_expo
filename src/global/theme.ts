391fd22c412762811bbe07f578050bc693929fd5
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-23 18:00:47 -0300

    feat: first commit

---
+++ src/global/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: '#0F5A65',
+    secondary: '#000',
+    secondary100: '#767171',
+    white: '#fff',
+
+    gradientPrimary: '#000000',
+    gradientSecondary: '#0F5A65',
+    gradientTerciary: '#000000',
+  },
+  fonts: {
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+  },
+};
