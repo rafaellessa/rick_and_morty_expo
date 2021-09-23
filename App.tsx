c67f563ac08448d8c56fd1abad4b45aae3ef6c97
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-23 18:30:46 -0300

    feat: app component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 import React from 'react';
-import { View } from 'react-native';
-
-
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/theme';
+import Routes from './src/Routes';
 
 const App: React.FC = () => {
-  return <View />;
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+   <ThemeProvider theme={theme}>
+      <NavigationContainer>
+      <Routes/>
+    </NavigationContainer>
+   </ThemeProvider>
+  )
 }
 
 export default App;
